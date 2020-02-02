530933d7b91562a8084d8e7b8961b7c07a08f2ce
Seamus Lee <seamuslee001@gmail.com>
2020-02-03 06:33:19 +1100

    Finish renameing process

--- CRM/Checkavailabletickets/Upgrader.php
+++ CRM/Checkavailabletickets/Upgrader.php
@@ -4,7 +4,7 @@ use CRM_Checkavailabletickets_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Checkavailabletickets_Upgrader extends CRM_Checkavaliabletickets_Upgrader_Base {
+class CRM_Checkavailabletickets_Upgrader extends CRM_Checkavailabletickets_Upgrader_Base {
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
