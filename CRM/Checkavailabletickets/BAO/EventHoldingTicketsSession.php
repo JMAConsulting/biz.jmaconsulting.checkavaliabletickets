53ea724c0c465af663401e1c55253eb709a77f4a
Seamus Lee <seamus.lee@jmaconsulting.biz>
2020-02-02 18:20:51 -0500

    Fix class name

--- CRM/Checkavailabletickets/BAO/EventHoldingTicketsSession.php
+++ CRM/Checkavailabletickets/BAO/EventHoldingTicketsSession.php
@@ -1,16 +1,16 @@
 <?php
-use CRM_Checkavaliabletickets_ExtensionUtil as E;
+use CRM_Checkavailabletickets_ExtensionUtil as E;
 
-class CRM_Checkavaliabletickets_BAO_EventHoldingTicketsSession extends CRM_Checkavaliabletickets_DAO_EventHoldingTicketsSession {
+class CRM_Checkavailabletickets_BAO_EventHoldingTicketsSession extends CRM_Checkavailabletickets_DAO_EventHoldingTicketsSession {
 
   /**
    * Create a new EventHoldingTicketsSession based on array-data
    *
    * @param array $params key-value pairs
-   * @return CRM_Checkavaliabletickets_DAO_EventHoldingTicketsSession|NULL
+   * @return CRM_Checkavailabletickets_DAO_EventHoldingTicketsSession|NULL
    *
   public static function create($params) {
-    $className = 'CRM_Checkavaliabletickets_DAO_EventHoldingTicketsSession';
+    $className = 'CRM_Checkavailabletickets_DAO_EventHoldingTicketsSession';
     $entityName = 'EventHoldingTicketsSession';
     $hook = empty($params['id']) ? 'create' : 'edit';
 
